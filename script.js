1177e11b434b40aad17708b192bfb0b003aa51b8
Jack Enneking <jackenneking@gmail.com>
2016-09-13 15:38:34 -0700

    Script and transitions

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 var main = function() {
+    $('.form-element').blur(function(event) {
+        if ($(this).val().length > 0) {
+            $(this).addClass('filled');
+        } else {
+            $(this).removeClass('filled');
+        }
+    });
+
+    $('.form-element').focus(function(event) {
+        $(this).removeClass('filled');
+    });
 
 }
 
